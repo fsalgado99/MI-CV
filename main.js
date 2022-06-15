ec85a2a5fbb33e3ad628c44cf5615cbd57c90792
k-ball99 <fsalgado199901@gmail.com>
2022-06-15 14:39:34 -0600

    subido de proyecto

---
+++ main.js
@@ -0,0 +1,34 @@
+$(document).ready(function()
+{
+    var contador=1;  /*necesario para hacer la animacion*/
+    alert('funciono'); 
+    $('.mobile-nav-toggle').click(function()
+    {
+        $('#header').toggle();
+        if(contador==1)
+        {
+            $('#hedaer').animate
+            (
+                {
+                    left:'0'
+
+                }
+            );
+            contador=0;
+        }
+        else
+        {       
+            contador=1;   
+                $('#hedaer').animate
+                (
+                    {
+                        left:'-100%'
+    
+                    }
+                );
+
+        }
+    })    
+
+});
+
